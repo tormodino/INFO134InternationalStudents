539a7df951fa927acd4229327da531aa5df8ef05
Vlummy <yvindj4@gmail.com>
2018-01-16 19:32:35 +0100

    Implementert map. Lagde en liten liste hvor vi skal ha dropdown meny for å teste om marker funksjonen min funker. Det gjør den så vi kan bytte denne listen med en fin dropdown

--- js/mapscript.js
+++ js/mapscript.js
@@ -3,6 +3,22 @@ var uib = {lat: 60.387819, lng: 5.321563}; // University of Bergen (default)
 var sc = {lat: 60.386707, lng: 5.323128}; // Student Center
 var uph = {lat: 60.388588, lng: 5.323424} // Ulrikke Pihls Hus
 
+// Adjustable marker position depending on user input
+var markerPosition = uib;
+
+function markerByInput(input) {
+  if(input == "uib") {
+    markerPosition = uib;
+  }
+  else if(input == "sc") {
+    markerPosition = sc;
+  }
+  else if (input == "uph") {
+    markerPosition = uph;
+  }
+  initMap();
+}
+
 /**
  * Javascript for a visible Google map
  * As Default the map centers at Norway/Bergen/University of Bergen (60.387819, 5.321563)
@@ -12,10 +28,10 @@ var uph = {lat: 60.388588, lng: 5.323424} // Ulrikke Pihls Hus
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
-    center: uib
+    center: markerPosition
   });
   var marker = new google.maps.Marker({
-    position: uib,
+    position: markerPosition,
     map: map
   });
 }
